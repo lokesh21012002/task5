387900961388a47766062875116f4e75574bd7ef
lokesh21012002 <83236804+lokesh21012002@users.noreply.github.com>
2021-06-03 10:54:59 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+// function fun()
+// {
+//   alert("Succesfully created");
+// }
+			var typed_strings =['','Lokesh'];
+      // $(".text-slider-items").text();
+
+    var typed = new Typed(".text-slider", {
+      strings: typed_strings,
+      typeSpeed: 70,
+      loop: true,
+      
+       backDelay: 900,
+      backSpeed: 70,
+    });
